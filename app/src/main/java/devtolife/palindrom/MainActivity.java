d8fd3d7510caff0cbf8b98c505dc210b4b155592
Vovas <marchenkoukr@gmail.com>
2017-12-11 23:08:41 +0200

    Some structure changes.

--- app/src/main/java/devtolife/palindrom/MainActivity.java
+++ app/src/main/java/devtolife/palindrom/MainActivity.java
@@ -8,17 +8,22 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int maxNum = 99999;
-    private int minNum = 10000;
+    private long maxNum = 99999;
+    private long minNum = 10000;
 
-    private int divNumMax = 0;
-    private int palind;
+
+    public long firstPalindrom = maxNum;
+    public long secondPalindrom = maxNum;
+
+    private long dividerNumMax = 0;
+    private long firstPrimeNum;
+    private long secondPrimeNum;
+    private long biggestPalindrom;
 
     private TextView tv1;
     private TextView tv2;
     private TextView tv3;
     private TextView tv4;
-    private TextView tv5;
 
 
     @Override
@@ -34,81 +39,117 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btnStart = (Button) findViewById(R.id.button);
         btnStart.setOnClickListener(this);
 
+        findMinNum();
+        findMaxNum();
+
+        findFirstPrimeNum();
+        findSecondPrimeNum();
+
+        multipleOfPrime();
+
     }
 
     @Override
     public void onClick(View v) {
 
-        divNumMax = (int) Math.sqrt(maxNum);
+        multipleOfPrime();
+        fillLayout();
 
-        int fPM;
-        int sPM;
-        boolean isNotPalind;
-
-        fPM = findMaxPrimeNumber(maxNum);
-        sPM = findMaxPrimeNumber(fPM - 2);
-        isNotPalind = findPalindrome(fPM, sPM);
-
-        while (isNotPalind) {
-
-            if (sPM <= fPM && sPM > minNum) {
-                sPM = findMaxPrimeNumber(sPM - 2);
-                isNotPalind = findPalindrome(fPM, sPM);
-
-            } else if (sPM <= minNum) {
-                fPM = findMaxPrimeNumber(fPM - 2);
-                sPM = fPM;
-            }
-
-            tv2.setText("1-st primary number: " + fPM);
-            tv3.setText("2-nd primary number: " + sPM);
-            tv4.setText("1-st * 2-nd = " + palind);
-
-        }
     }
 
+    private long findNextPrimeNum(long previousNum) {
+        long i;
+        long j;
+        long z;
+        long maxNumNew;
 
-    private int findMaxPrimeNumber(int maxNumPre) {
-        int i;
-        int j;
-        int z;
-        int maxNumNew;
+        findDividerNumMax(previousNum);
 
-        for (i = maxNumPre; i >= minNum; i = i - 2) {
+        for (i = previousNum; i >= minNum; i = i - 2) {
 
-            for (j = 3; j <= divNumMax; j++) {
+            for (j = 3; j <= dividerNumMax; j++) {
 
                 z = i % j;
 
-                if (z == 0 && j <= divNumMax) {
+                if (z == 0 && j < dividerNumMax) {
                     break;
 
-                } else if (z != 0 && j == divNumMax) {
+                } else if (z != 0 && j >= dividerNumMax) {
                     maxNumNew = i;
                     return maxNumNew;
-
                 }
+            }
+        }
+        return 1;
+    }
+
+    public void findDividerNumMax(long maxNumPreD) {
+
+        dividerNumMax = (long) Math.sqrt(maxNumPreD);
+    }
+
+    private void findPalindrome(long firstPrime, long secondPrime) {
+
+        long resultOfMath = firstPrime * secondPrime;
+
+        String ltrResult = Long.toString(resultOfMath);
+        String rtlResult = new StringBuilder(ltrResult).reverse().toString();
+
+        if (ltrResult.equals(rtlResult) && resultOfMath > biggestPalindrom) {
+            biggestPalindrom = resultOfMath;
+            firstPrimeNum = firstPrime;
+            secondPrimeNum = secondPrime;
+
+        }
+    }
+
+
+    public void findMinNum() {
+        minNum = 10000;
+    }
+
+    public void findMaxNum() {
+        maxNum = 99999;
+    }
+
+
+    public void findFirstPrimeNum() {
+    }
+
+    public void findSecondPrimeNum() {
+    }
+
+    public void multipleOfPrime() {
+
+        boolean isNotMaxPalind = true;
+
+
+        while (isNotMaxPalind) {
+            if (firstPalindrom == maxNum && secondPalindrom == maxNum) {
+                firstPalindrom = findNextPrimeNum(maxNum);
+                secondPalindrom = findNextPrimeNum(firstPalindrom - 2);
+                findPalindrome(firstPalindrom, secondPalindrom);
+
+            } else if (secondPalindrom <= firstPalindrom && secondPalindrom > minNum) {
+                secondPalindrom = findNextPrimeNum(secondPalindrom - 2);
+                findPalindrome(firstPalindrom, secondPalindrom);
+
+            } else if (secondPalindrom <= minNum) {
+                firstPalindrom = findNextPrimeNum(firstPalindrom - 2);
+                secondPalindrom = firstPalindrom;
+
+            } else if (firstPalindrom <= minNum) {
+                isNotMaxPalind = false;
             }
 
         }
-        return 10000;
+
     }
 
-    private boolean findPalindrome(int firstPrime, int secondPrime) {
-
-        int resultOfMath = firstPrime * secondPrime;
-
-        String ltrResult = Integer.toString(resultOfMath);
-        String rtlResult = new StringBuffer(ltrResult).reverse().toString();
-
-        if (ltrResult.equals(rtlResult)) {
-
-            palind = resultOfMath;
-            return false;
-
-        } else {
-            return true;
-        }
+    public void fillLayout() {
+        tv2.setText("1-st primary number: " + firstPrimeNum);
+        tv3.setText("2-nd primary number: " + secondPrimeNum);
+        tv4.setText("1-st * 2-nd = " + biggestPalindrom);
     }
 }
 
